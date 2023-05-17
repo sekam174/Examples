93a1f87985ca9ff1ff7d1a9a43b04bbbb6a79d66
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-17 23:40:13 +0300

    Exsample 005 ConditionIfEise

---
+++ Exsample005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вика")
+{
+    Console.WriteLine("Ура! это же Вика!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
