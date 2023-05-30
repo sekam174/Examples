14ecfe886fe183008d93fd835125fb613fc17673
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-30 17:53:21 +0300

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 41, 54, 83, 54, 45, 26, 37, 68, 19};
+
+int n = array.Length;
+int find = 54;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
